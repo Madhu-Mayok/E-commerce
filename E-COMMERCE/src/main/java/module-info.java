1b96e0b485b44afa0862cca21f874270acad12a1
Madhu-Mayok <117559130+Madhu-Mayok@users.noreply.github.com>
2023-04-28 11:30:22 +0530

    Delete E-COMMERCE directory

--- E-COMMERCE/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.ecommerce {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.ecommerce to javafx.fxml;
-    exports com.example.ecommerce;
-}
